e8953df734837cc4369bcdaa074e006ff77629b6
kirillturok <71722037+kirillturok@users.noreply.github.com>
2021-06-21 23:57:40 +0300

    Update DateFormatter.kt

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -4,6 +4,13 @@ class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM, EEEE")
+            .withLocale(Locale.forLanguageTag("Ru"));
+        try {
+            return LocalDate.of(year.toInt(),month.toInt(),day.toInt()).format(formatter);
+        }catch (e:Exception){
+            return "Такого дня не существует"
+        }
         throw NotImplementedError("Not implemented")
     }
 }
