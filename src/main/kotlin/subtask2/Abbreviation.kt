f2d500f25f55e5bdfd369f225c70ce4a984480e6
kirillturok <71722037+kirillturok@users.noreply.github.com>
2021-06-21 23:57:16 +0300

    Update Abbreviation.kt

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,18 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
+        val a = a.toLowerCase()
+        val b = b.toLowerCase()
+        var indexB = 0;  // позиция буквы в строке "b"
+        for (letter in a) {
+            if (letter == b[indexB]) {
+                if (indexB >= b.lastIndex) {
+                    return "YES"
+                }
+                indexB++
+            }
+        }
+        return "NO"
         throw NotImplementedError("Not implemented")
     }
 }
