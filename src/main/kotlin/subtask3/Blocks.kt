bfcdf41d8b0b437ed914ee87ce810a0d8a4ecd4d
kirillturok <71722037+kirillturok@users.noreply.github.com>
2021-06-21 23:56:49 +0300

    Update Blocks.kt

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -6,6 +6,35 @@ class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+        fun sumInt (array: Array<*>): Int
+        {
+            var sum = 0
+            for (i in array)
+                if (i is Int)
+                    sum+=i
+            return sum
+        }
+
+        fun strings (array: Array<*>):String
+        {
+            var word = ""
+            for (i in array)
+                if (i is String)
+                    word = word.plus(i)
+            return word
+        }
+        when (blockB) {
+            Int::class -> return sumInt(blockA)
+            String::class -> return strings(blockA)
+            LocalDate::class -> {
+                var date = LocalDate.MIN
+                for (i in blockA)
+                    if (i is LocalDate)
+                        if (date.isBefore(i))
+                            date = i
+                return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            }
+        }
         throw NotImplementedError("Not implemented")
     }
 }
